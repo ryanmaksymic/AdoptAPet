ac170af7a9620e3c5b65d6db9b8077199534c6d0
ryanmaksymic <ryanmaksymic@gmail.com>
2018-02-08 19:49:37 -0500

    Set up Search storyboard. Started linking search view to SearchViewController. Created SearchResultsViewController to display search results in both list and map views.

---
+++ AdoptAPet/SearchResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultsViewController.h
+//  AdoptAPet
+//
+//  Created by Ryan Maksymic on 2018-02-08.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultsViewController : UIViewController
+
+@end
--- AdoptAPet/SearchViewController.m
+++ AdoptAPet/SearchViewController.m
@@ -10,28 +10,20 @@
 
 @interface SearchViewController ()
 
+
 @end
+
 
 @implementation SearchViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)buttonToggled:(UIButton *)sender
+{
+  sender.selected = !sender.selected;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
